3f4df1ceb0f6cf89b727b6161b87dce907e74571
sonali_sohoni <ssohoni@camsbycbs.com>
2022-01-27 20:09:51 -0500

    productlist and productitem changes

--- client/src/App.js
+++ client/src/App.js
@@ -1,45 +1,46 @@
-import React from 'react';
-import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import React from "react";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import {
-  ApolloClient,
-  InMemoryCache,
-  ApolloProvider,
-  createHttpLink,
-} from '@apollo/client';
-import { setContext } from '@apollo/client/link/context';
+	ApolloClient,
+	InMemoryCache,
+	ApolloProvider,
+	createHttpLink,
+} from "@apollo/client";
+import { setContext } from "@apollo/client/link/context";
 
-import Home from './pages/Home';
-import Detail from './pages/Detail';
-import NoMatch from './pages/NoMatch';
-import Login from './pages/Login';
-import Signup from './pages/Signup';
-import Nav from './components/Nav';
+import Home from "./pages/Home";
+import Detail from "./pages/Detail";
+import NoMatch from "./pages/NoMatch";
+import Login from "./pages/Login";
+import Signup from "./pages/Signup";
+import Nav from "./components/Nav";
 //import { StoreProvider } from './utils/GlobalState';
 import { Provider } from "react-redux";
-import Success from './pages/Success';
-import OrderHistory from './pages/OrderHistory';
+import store from "./utils/store";
+import Success from "./pages/Success";
+import OrderHistory from "./pages/OrderHistory";
 
 const httpLink = createHttpLink({
-  uri: '/graphql',
+	uri: "/graphql",
 });
 
 const authLink = setContext((_, { headers }) => {
-  const token = localStorage.getItem('id_token');
-  return {
-    headers: {
-      ...headers,
-      authorization: token ? `Bearer ${token}` : '',
-    },
-  };
+	const token = localStorage.getItem("id_token");
+	return {
+		headers: {
+			...headers,
+			authorization: token ? `Bearer ${token}` : "",
+		},
+	};
 });
 
 const client = new ApolloClient({
-  link: authLink.concat(httpLink),
-  cache: new InMemoryCache(),
+	link: authLink.concat(httpLink),
+	cache: new InMemoryCache(),
 });
 
 function App() {
-  return (
+	return (
 		<ApolloProvider client={client}>
 			<Router>
 				<div>
--- client/src/components/CategoryMenu/index.js
+++ client/src/components/CategoryMenu/index.js
@@ -1,61 +1,65 @@
-import React, { useEffect } from 'react';
-import { useQuery } from '@apollo/client';
-import { useStoreContext } from '../../utils/GlobalState';
+import React, { useEffect } from "react";
+import { useQuery } from "@apollo/client";
+//import { useStoreContext } from '../../utils/GlobalState';
+import { useDispatch, useSelector } from "react-redux";
 import {
-  UPDATE_CATEGORIES,
-  UPDATE_CURRENT_CATEGORY,
-} from '../../utils/actions';
-import { QUERY_CATEGORIES } from '../../utils/queries';
-import { idbPromise } from '../../utils/helpers';
+	UPDATE_CATEGORIES,
+	UPDATE_CURRENT_CATEGORY,
+} from "../../utils/actions";
+import { QUERY_CATEGORIES } from "../../utils/queries";
+import { idbPromise } from "../../utils/helpers";
 
 function CategoryMenu() {
-  const [state, dispatch] = useStoreContext();
+	//const [state, dispatch] = useStoreContext();
+	const dispatch = useDispatch();
 
-  const { categories } = state;
+	const state = useSelector((state) => state);
 
-  const { loading, data: categoryData } = useQuery(QUERY_CATEGORIES);
+	const { categories } = state;
 
-  useEffect(() => {
-    if (categoryData) {
-      dispatch({
-        type: UPDATE_CATEGORIES,
-        categories: categoryData.categories,
-      });
-      categoryData.categories.forEach((category) => {
-        idbPromise('categories', 'put', category);
-      });
-    } else if (!loading) {
-      idbPromise('categories', 'get').then((categories) => {
-        dispatch({
-          type: UPDATE_CATEGORIES,
-          categories: categories,
-        });
-      });
-    }
-  }, [categoryData, loading, dispatch]);
+	const { loading, data: categoryData } = useQuery(QUERY_CATEGORIES);
 
-  const handleClick = (id) => {
-    dispatch({
-      type: UPDATE_CURRENT_CATEGORY,
-      currentCategory: id,
-    });
-  };
+	useEffect(() => {
+		if (categoryData) {
+			dispatch({
+				type: UPDATE_CATEGORIES,
+				categories: categoryData.categories,
+			});
+			categoryData.categories.forEach((category) => {
+				idbPromise("categories", "put", category);
+			});
+		} else if (!loading) {
+			idbPromise("categories", "get").then((categories) => {
+				dispatch({
+					type: UPDATE_CATEGORIES,
+					categories: categories,
+				});
+			});
+		}
+	}, [categoryData, loading, dispatch]);
 
-  return (
-    <div>
-      <h2>Choose a Category:</h2>
-      {categories.map((item) => (
-        <button
-          key={item._id}
-          onClick={() => {
-            handleClick(item._id);
-          }}
-        >
-          {item.name}
-        </button>
-      ))}
-    </div>
-  );
+	const handleClick = (id) => {
+		dispatch({
+			type: UPDATE_CURRENT_CATEGORY,
+			currentCategory: id,
+		});
+	};
+
+	return (
+		<div>
+			<h2>Choose a Category:</h2>
+			{categories.map((item) => (
+				<button
+					key={item._id}
+					onClick={() => {
+						handleClick(item._id);
+					}}
+				>
+					{item.name}
+				</button>
+			))}
+		</div>
+	);
 }
 
 export default CategoryMenu;
