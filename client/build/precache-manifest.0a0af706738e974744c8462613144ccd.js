553b469cb43f66427da376447132237ec72f48cf
sonali_sohoni <ssohoni@camsbycbs.com>
2022-01-29 21:39:37 -0500

    building

---
+++ client/build/precache-manifest.0a0af706738e974744c8462613144ccd.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b4a21607ba4f41964b2d36c75ae1fc8d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "0d50b5344846329746e6",
+    "url": "/static/css/main.e213c893.chunk.css"
+  },
+  {
+    "revision": "408a40d144754493fb0d",
+    "url": "/static/js/2.43ea1bae.chunk.js"
+  },
+  {
+    "revision": "f333c948d07dc08e5747a9b83a79e23a",
+    "url": "/static/js/2.43ea1bae.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0d50b5344846329746e6",
+    "url": "/static/js/main.e090d988.chunk.js"
+  },
+  {
+    "revision": "02751e160f988a50d44f",
+    "url": "/static/js/runtime-main.8a258a78.js"
+  },
+  {
+    "revision": "ab4978553bd68ca15e503a0da7fa604d",
+    "url": "/static/media/spinner.ab497855.gif"
+  }
+]);
